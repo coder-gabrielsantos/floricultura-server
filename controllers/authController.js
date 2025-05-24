4c9611d13bbe91e93ed85dd77244b4e3d6f921cc
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-05-24 14:38:01 -0300

    support identifier field in user registration by mapping to email or phone

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,7 +37,13 @@ exports.register = async (req, res) => {
 
         await newUser.save();
 
-        res.status(201).json({ message: "User registered successfully" });
+        res.status(201).json({
+            _id: newUser._id,
+            name: newUser.name,
+            email: newUser.email,
+            phone: newUser.phone,
+            role: newUser.role
+        });
     } catch (err) {
         res.status(500).json({ message: "Error registering user", error: err });
     }

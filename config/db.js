db3cd15d190e3ba30d0c7e2a2ddad28d8a1fa424
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-05-25 09:01:43 -0300

    refactoring

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("✅ Connected to MongoDB");
+        console.log("Connected to MongoDB");
     } catch (err) {
-        console.error("❌ Failed to connect to MongoDB:", err.message);
+        console.error("Failed to connect to MongoDB:", err.message);
         process.exit(1);
     }
 };
